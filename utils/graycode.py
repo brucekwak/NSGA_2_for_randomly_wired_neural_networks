cbd9040723f8930f27343380ba19453eaf56fdf8
brucekwak <777kwak@gmail.com>
2020-04-01 20:19:48 +0900

    erase unnecessary code

--- utils/graycode.py
+++ utils/graycode.py
@@ -2,12 +2,6 @@ from __future__ import division
 import random
 import warnings
 
-##### #####
-# try:
-#     from collections.abc import Sequence
-# except ImportError:
-#     from collections import Sequence
-# from itertools import repeat
 
 def check_Upper(gy, num_graph):
     tmp = ''
@@ -59,22 +53,6 @@ def num2gray(n, gray_len):
 
     return gy
 
-##### #####
-# def cxOnePoint_gray(ind1, ind2):
-#     """Executes a one point crossover on the input :term:`sequence` individuals.
-#     The two individuals are modified in place. The resulting individuals will
-#     respectively have the length of the other.
-#     :param ind1: The first individual participating in the crossover.
-#     :param ind2: The second individual participating in the crossover.
-#     :returns: A tuple of two individuals.
-#     This function uses the :func:`~random.randint` function from the
-#     python base :mod:`random` module.
-#     """
-#     size = min(len(ind1), len(ind2))
-#     cxpoint = random.randint(1, size - 1)
-#     ind1[cxpoint:], ind2[cxpoint:] = ind2[cxpoint:], ind1[cxpoint:]
-
-#     return ind1, ind2
 
 ############################
 # Crossover - for gray encoding
